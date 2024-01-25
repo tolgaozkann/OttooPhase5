659ad6369d8448a978dff1befa46e7dfed67361c
tolgaozkann <ozkanozkan3233@gmail.com>
2024-01-25 14:16:11 +0300

    Add project files.

---
+++ Ottoo.Entities/Ottoo.Entities.Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ottoo.Entities.Ottoo.Entities.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+
+        //navigation property
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
---
+++ Ottoo.Entities/Ottoo.Entities.Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ottoo.Entities.Ottoo.Entities.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        //navigation property
+        public ICollection<Book> Books { get; set; }
+    }
+}
