0c0e758ee28add70a15d7be7f9fa6e6b81e77699
tolgaozkann <ozkanozkan3233@gmail.com>
2024-01-29 14:22:15 +0300

    API LAYER ADDED

---
+++ Ottoo.WebAPI/Controllers/BookController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ottoo.Entities.Dtos.Book;
+using Ottoo.Services.Contracts;
+using System.Text.Json;
+
+namespace Ottoo.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IServiceManager _manager;
+
+        public BookController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet(Name = "GetAllBooks")]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            var result = await _manager.BookService.GetAllBooksAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetOneBook([FromRoute(Name = "id")] int id)
+        {
+            return Ok(await _manager.BookService.GetOneBookByIdAsync(id));
+        }
+
+        [HttpPost(Name = "CreateOneBook")]
+        public async Task<IActionResult> CreateOneBook([FromBody] InsertBookDto bookDto)
+        {
+            var book = await _manager.BookService.CreateOneBookAsync(bookDto);
+            return StatusCode(201, book);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] UpdateBookDto book)
+        {
+            if (book is null)
+                return BadRequest("Request body is null");
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            await _manager.BookService.UpdateOneBookAsync(id, book);
+
+            return NoContent();//204
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteOneBook([FromRoute(Name = "id")] int id)
+        {
+            await _manager.BookService.DeleteOneBookAsync(id);
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Ottoo.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ottoo.Entities.Dtos.Category;
+using Ottoo.Entities.Dtos.User;
+using Ottoo.Services.Contracts;
+
+namespace Ottoo.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public UserController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpGet(Name ="GetAllUsers")]
+        public async Task<IActionResult> GetAllUsersAsync()
+        {
+            var users = await _serviceManager.UserService.GetAllUsersAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetOneUserByIdAsync([FromRoute] int id)
+        {
+            var user = await _serviceManager.UserService.GetOneUserByIdAsync(id);
+
+            if(user is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost(Name = "CreateOneUser")]
+        public async Task<IActionResult> CreateOneUserAsync([FromBody] UserRegisterDto userRegisterDto)
+        {
+            var result = await _serviceManager.UserService.CreateOneUserAsync(userRegisterDto);
+
+            return StatusCode(201, result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteOneUserByIdAsync([FromRoute] int id)
+        {
+            await _serviceManager.UserService.DeleteOneUserAsync(id);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateOneUserAsync([FromRoute] int id, [FromBody] UserDto user)
+        {
+            if (user is null)
+                return BadRequest("Request body is null");
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+            await _serviceManager.UserService.UpdateOneUserAsync(id, user);
+
+            return NoContent();
+        }
+    }
+}
